8c6b3841874730074d3636316e30f88c2dce9ec2
abhisaharan <abhi.saharan@gmail.com>
2017-10-17 02:41:09 -0700

    form validations done

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class SignupForm(Form):
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('email')
-    password = PasswordField('password')
+    first_name = StringField('First name', validators=[DataRequired("Please enter your first name")])
+    last_name = StringField('Last name', validators=[DataRequired("Please enter your last name")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email"), Email("Dont leave Email blank please")])
+    password = PasswordField('password', validators=[DataRequired("Please enter password"), Length(min=6, message="It must be more than six characters long")])
     submit = SubmitField('Sign up')
